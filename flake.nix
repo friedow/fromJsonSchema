0ec09e406261489c233e1c89043b12c0e98bd491
Christian Friedow <christian@friedow.com>
2025-10-14 21:30:21 +0200

    test: test conversion of markdownlint json schema

--- flake.nix
+++ flake.nix
@@ -2,8 +2,19 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
   outputs =
-    { nixpkgs, ... }:
+    { self, nixpkgs, ... }:
     {
       lib.fromJsonSchema = (import ./fromJsonSchema.nix { inherit nixpkgs; });
+
+      tests.convertMarkdownlintConfig = self.lib.fromJsonSchema (
+        builtins.fromJSON (
+          builtins.readFile (
+            builtins.fetchurl {
+              sha256 = "04wbgrzl3d6mdnvqi8142gz69006hjvrwhd7gvkx7wyqqkw9rpj4";
+              url = "https://raw.githubusercontent.com/DavidAnson/markdownlint/main/schema/markdownlint-config-schema.json";
+            }
+          )
+        )
+      );
     };
 }
